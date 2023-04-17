e1e17b67c7d617225a5b2e31c406bbdf89bcad58
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-04-17 12:03:16 +0900

    SetCursorPosition 연습

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("메서드 호출");
+            Console.SetCursorPosition(5, 5);
+            Console.Write("메서드 호출2");
+            Console.SetCursorPosition(2, 2);
+            Console.Write("메서드 호출3");
+        }
+    }
+}
